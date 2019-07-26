f8b27535673f0ed3a29f6592052f7b5dac2e4742
fonte-nele <felipe.fontenele.op@gmail.com>
2019-07-26 11:42:41 -0300

    #feat: versão 1.2

--- app.js
+++ app.js
@@ -31,7 +31,9 @@ app.use(bodyParser.json());
 app.use(methodOverride('_method'));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  //res.sendFile(__dirname + "/views/index.html");
+  //res.
+  res.send('Login');
 });
 
 // Login Form Post
@@ -44,7 +46,9 @@ app.post('/', (req, res, next) => {
 });
 
 app.get("/insumos", (req, res) => {
-  res.sendFile(__dirname + "/views/insumo/addInsumos.html");
+  //res.sendFile(__dirname + "/views/insumo/addInsumos.html");
+
+  res.send('Pagina de Insumos');
 });
 
 app.post("/insumos", (req, res) => {
@@ -65,13 +69,14 @@ app.post("/insumos", (req, res) => {
 
   if (errors.length > 0) {
     console.log("Erro: " + errors),
-      res.render('/insumos', {
-        errors: errors,
-        categoria: req.body.categoria,
-        descricao: req.body.descricao,
-        estoque: req.body.estoque,
-        capacidade: req.body.capacidade
-      });
+      res.send('Error');
+    /*res.render('/insumos', {
+      errors: errors,
+      categoria: req.body.categoria,
+      descricao: req.body.descricao,
+      estoque: req.body.estoque,
+      capacidade: req.body.capacidade
+    });*/
   }
   else {
     const newInsumo = {
@@ -86,7 +91,8 @@ app.post("/insumos", (req, res) => {
       .save()
       .then(insumo => {
         console.log('sucesso'),
-          res.redirect('/insumos');
+          res.send('Insumo cadastrado com sucesso.');
+        //res.redirect('/insumos');
       })
   }
 });
@@ -99,15 +105,16 @@ app.put('/insumos/:id', (req, res) => {
     .then(insumo => {
       // new values
       insumo.nome = req.body.nome,
-      insumo.categoria = req.body.categoria,
-      insumo.descricao = req.body.descricao,
-      insumo.estoque = req.body.estoque,
-      insumo.capacidade = req.body.capacidade
+        insumo.categoria = req.body.categoria,
+        insumo.descricao = req.body.descricao,
+        insumo.estoque = req.body.estoque,
+        insumo.capacidade = req.body.capacidade
 
       insumo.save()
         .then(insumo => {
           req.flash('success_msg', 'Insumo atualizado');
-          res.redirect('/insumos');
+          //res.redirect('/insumos');
+          res.send('Insumo editado com sucesso.');
         })
     });
 });
@@ -117,7 +124,8 @@ app.delete('/insumos/:id', (req, res) => {
   Insumo.remove({ _id: req.params.id })
     .then(() => {
       req.flash('success_msg', 'Insumo removido');
-      res.redirect('/insumos');
+      //res.redirect('/insumos');
+      res.send('Insumo excluido com sucesso');
     });
 });
 
